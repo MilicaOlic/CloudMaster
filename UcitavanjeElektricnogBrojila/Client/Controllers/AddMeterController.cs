5b5aadb9dcd8e9bbb541ff0abcad64c82475f10e
MilicaOlic <79918889+MilicaOlic@users.noreply.github.com>
2023-03-25 23:30:21 +0100

    Initial setup

---
+++ UcitavanjeElektricnogBrojila/Client/Controllers/AddMeterController.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Metrics;
+
+namespace Client.Controllers
+{
+    public class AddMeterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddMeterView()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/AddMeter/AddMeterMethod")]
+        public Task<IActionResult>AddMeter(MeterDevice meter) 
+        {
+            return Task.FromResult<IActionResult>(View("Index"));
+        }
+    }
+}
---
+++ UcitavanjeElektricnogBrojila/Common/MeterDevice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class MeterDevice
+    {
+        public MeterDevice(string meterId, string meterState)
+        {
+            MeterId = meterId;
+            MeterState = meterState;
+        }
+        public MeterDevice()
+        { }
+
+        [Key]
+        [DataMember]
+        public string MeterId { get; set; }
+        [DataMember]
+        public string MeterState { get; set; }
+
+
+    }
+}
